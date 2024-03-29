31661e4fa5de8d3043e06ec8c2c4fc3de7674d37
brunomry <brunomadozzo@gmail.com>
2024-03-29 01:31:57 -0300

    Agregue img de fondo para main + seccion de bienvenida para admin con 3 opciones de botones para gestion de productos, usuarios y pedidos estilizando con clases de bootstrap y estilos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Administracion from "./components/pages/Administracion";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
   return (
     <>
+      <Administracion></Administracion>
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/pages/Administracion.jsx
+++ src/components/pages/Administracion.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import "../../styles/administrador.css";
 
 const Administracion = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="mainSection m-0">
+      <h1 className="text-center my-5">Bienvenido "Administrador"</h1>
+      <article className="me-2 mx-sm-auto d-flex justify-content-center border border border-1 border-dark managementOptions px-3 px-sm-5">
+        <div className="d-flex gap-3 flex-column justify-content-center align-items-center">
+          <button
+            className="border border-1 border-white fw-bold py-3 btnManagement rounded-2"
+          >
+            Gestionar Productos <i class="fa-solid fa-pizza-slice"></i>
+          </button>
+          <button className="border border-1 border-white fw-bold py-3 btnManagement rounded-2">
+            Gestionar Usuarios <i class="fa-solid fa-user"></i>
+          </button>
+          <button className="border border-1 border-white fw-bold py-3 btnManagement rounded-2">
+            Gestionar Pedidos <i class="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
+      </article>
+    </section>
   );
 };
 
