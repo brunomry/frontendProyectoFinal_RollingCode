ba4416b7ed4d439d0ab2804605e09c362ae67ba8
Fernando Herrera <herrera770@outlook.es>
2024-03-29 21:55:28 -0300

    se agregaron colores distintivos al header y navbar. Tambien se conifguró el navbar para que cuando sea un botón hamburguesa las opcione desplegadas aparezcan centradas

--- src/components/common/MenuNavegacion.jsx
+++ src/components/common/MenuNavegacion.jsx
@@ -4,37 +4,45 @@ import { Button, Container, Nav, Navbar } from "react-bootstrap";
 const MenuNavegacion = () => {
   return (
     <>
-      <header className="p-3 d-none d-md-block d-md-flex d-lg-flex d-xl-flex d-xxl-flex justify-content-between">
+      <header className="p-3 d-none d-md-block d-md-flex d-lg-flex d-xl-flex d-xxl-flex justify-content-between align-items-center">
         <div>
-          <i className="fa-brands fa-facebook-f fa-2xl me-3"> </i>{" "}
-          <i className="fa-brands fa-instagram fa-2xl me-3"></i>{" "}
-          <i class="fa-brands fa-x-twitter fa-2xl"></i>
+          <i className="fa-brands fa-facebook-f fa-2xl me-3"> </i>
+          <i className="fa-brands fa-instagram fa-2xl me-3"></i>
+          <i className="fa-brands fa-x-twitter fa-2xl"></i>
         </div>
         <div>
           <h1>MARCA/LOGO</h1>
         </div>
         <div>
           <p>
-            {" "}
-            <i class="fa-brands fa-whatsapp fa-2xl"></i> 123-1234567
+            <i className="fa-brands fa-whatsapp fa-2xl"></i> 123-1234567
           </p>
         </div>
       </header>
-      <Navbar expand="lg" className="bg-body-tertiary navLinks">
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
+        <Navbar expand="lg"  className="navLinks navbarBackground mt-3">
+          <Container>
+            <Navbar.Brand href="#home" className="d-block d-md-none">
+              MARCA/LOGO
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto align-items-center">
                 <Nav.Link href="#">Inicio</Nav.Link>
                 <Nav.Link href="#">Menu</Nav.Link>
                 <Nav.Link href="#">Nosotros</Nav.Link>
                 <Nav.Link href="#">Contacto</Nav.Link>
-                <Nav.Link href="#">Mi cuenta</Nav.Link>
-                <Nav.Link href="#">Mi pedido</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+                <Nav.Link href="#">
+                  {" "}
+                  <i className="fa-regular fa-circle-user fa-2xl"></i> Mi cuenta
+                </Nav.Link>
+                <Nav.Link href="#">
+                  {" "}
+                  <i className="fa-solid fa-cart-shopping fa-2xl"></i> Mi pedido
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
     </>
   );
 };
