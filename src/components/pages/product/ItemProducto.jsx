50bb76c31c9a0f7dc1b6fb4c671c5034dd74d297
brunomry <brunomadozzo@gmail.com>
2024-03-30 01:45:01 -0300

    Agregue fila para tabla en cp ItemProducto y btn nuevo producto en cp ListaProductos con mismos estilos que btn nuevo usuario

--- src/components/pages/product/ItemProducto.jsx
+++ src/components/pages/product/ItemProducto.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import "../../../styles/administrador.css";
+import { Button } from "react-bootstrap";
 
 const ItemProducto = () => {
   return (
-    <></>
+    <tr className="align-middle">
+      <td>pizza de metro</td>
+      <td>Pizzas</td>
+      <td>4500</td>
+      <td>
+        <img
+          src="https://images.pexels.com/photos/162744/tomatoes-tomato-quiche-red-yellow-162744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="imgProduct"
+        />
+      </td>
+      <td>Disponible</td>
+      <td className="d-flex flex-column gap-2 align-items-center flex-lg-row d-lg-table-cell">
+        <Link
+          title="Ver producto"
+          to={`/administrador/verProducto/id`}
+          className="me-lg-2 btn btn-secondary text-dark"
+        >
+          <i className="fa-solid fa-eye"></i>
+        </Link>
+        <Link
+          title="Editar producto"
+          to={`/administrador/editarProducto/id`}
+          className="me-lg-2 btn btn-warning"
+        >
+          <i className="fa-solid fa-pen-to-square"></i>
+        </Link>
+        <Button variant="danger" title="Eliminar producto" className="me-lg-2">
+          <i className="fa-solid fa-trash text-dark"></i>
+        </Button>
+      </td>
+    </tr>
   );
 };
 
--- src/components/pages/product/ListaProductos.jsx
+++ src/components/pages/product/ListaProductos.jsx
@@ -1,12 +1,19 @@
 import { Table } from "react-bootstrap";
 import "../../../styles/administrador.css";
-import ItemProducto from "./ItemProducto.jsx"
+import ItemProducto from "./ItemProducto.jsx";
+import { Link } from "react-router-dom";
 
 const ListaProductos = () => {
   return (
     <section className="mainSection py-5">
       <div className="container d-flex justify-content-between align-items-center flex-wrap gap-2">
-        <h1>Gestión de Productos</h1>
+        <h1>Gestión de productos</h1>
+        <Link
+          className="btnNewProduct border border-1 border-white fw-bold p-2 rounded-2 text-decoration-none"
+          to="/administrador/crearProducto"
+        >
+          <i className="fa-solid fa-plus"></i> Nuevo producto
+        </Link>
       </div>
       <hr className="container" />
       <Table
@@ -25,6 +32,13 @@ const ListaProductos = () => {
           </tr>
         </thead>
         <tbody>
+          <ItemProducto></ItemProducto>
+          <ItemProducto></ItemProducto>
+          <ItemProducto></ItemProducto>
+          <ItemProducto></ItemProducto>
+          <ItemProducto></ItemProducto>
+          <ItemProducto></ItemProducto>
+          <ItemProducto></ItemProducto>
           <ItemProducto></ItemProducto>
         </tbody>
       </Table>
