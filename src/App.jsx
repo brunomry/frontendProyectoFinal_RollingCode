0313cb3adce0b75929771b1680a989d495207ad9
Fernando Herrera <herrera770@outlook.es>
2024-03-30 17:48:50 -0300

    se creó un banner y un selector de categorías a modo de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-
+import Menu from "./components/pages/Menu"
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 
   return (
     <>
+    <Menu></Menu>
     </>
   )
 }
--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import "../../styles/menu.css";
+import  bannerMenu from "../../assets/bannerMenuProvisorio.png"
 const Menu = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="text-center bannerMenu d-flex flex-column justify-content-center">
+        <h1>Nuestro Menu</h1>
+        <p>elegí el plato que más prefieras</p>
+        <a href="#">descargar el menú</a>
+      </section>
+      <div className="border-bottom border-black d-flex justify-content-center p-4">
+        <p> <a className="link-offset-2 link-underline link-underline-opacity-0" href="#">Categoría 1</a> </p>
+        <p> <a className="link-offset-2 link-underline link-underline-opacity-0" href="#">Categoría 2</a> </p>
+        <p> <a className="link-offset-2 link-underline link-underline-opacity-0" href="#">Categoría 3</a> </p>
+        <p> <a className="link-offset-2 link-underline link-underline-opacity-0" href="#">Categoría 4</a> </p>
+      </div>
+    </>
   );
 };
 
