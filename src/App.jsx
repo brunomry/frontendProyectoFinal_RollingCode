e84033fe97182e38fabb76b994ba67b4223056f5
Fernando Herrera <herrera770@outlook.es>
2024-03-31 17:14:24 -0300

    se agregó una lista de rutas provisional en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import MenuNavegacion from "./components/common/MenuNavegacion"
+import MenuNavegacion from "./components/common/MenuNavegacion";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
-    <MenuNavegacion></MenuNavegacion>
+      <BrowserRouter>
+        <MenuNavegacion></MenuNavegacion>
+        <Routes>
+          <Route exact path="/" element="#"></Route>
+          <Route exact path="/menu" element="#"></Route>
+          <Route exact path="/administracion" element="#"></Route>
+          <Route exact path="/login" element="#"></Route>
+          <Route exact path="/registro" element="#"></Route>
+          <Route exact path="/pedido" element="#"></Route>
+          <Route exact path="/nosotros" element="#"></Route>
+          <Route exact path="/login" element="#"></Route>
+          <Route exact path="/detalleProducto/:id" element="#"></Route>
+          <Route path="*" element="error 404"></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
