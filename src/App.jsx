e3e1b7fad74c854d90b31bbcc130f7c0eae6c884
brunomry <brunomadozzo@gmail.com>
2024-03-31 14:47:41 -0300

    Agregue seccion banner en cp Inicio con direccion, nombre de restaurante, descripcion y btn de Menu con estilos css y clases de bootstrap en index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Inicio from "./components/pages/Inicio";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio></Inicio>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { Button } from "react-bootstrap";
+import "../../styles/index.css";
+import { Link } from "react-router-dom";
 
 const Inicio = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className="banner">
+        <div className="bannerOpacity"></div>
+        <article className="bannerContent pb-md-5">
+          <small className="fw-bold text-center"><i class="fa-solid fa-location-dot"></i> General Paz 576, San Miguel de Tucumán, Tucumán</small>
+          <div>
+            <p className="address fs-5 fw-bold">- Restaurante -</p>
+            <h1 className="display-2">Ambiente Bohemio</h1>
+          </div>
+          <p className="fs-5 fw-bold">Te esperamos para una experiencia gastronómica inolvidable.</p>
+          <Link className="border border-3 border-white rounded-5 px-2 px-md-3 py-3 text-decoration-none text-white btnBanner fw-bold fs-5">
+            CONOCE NUESTRO MENÚ
+          </Link>
+        </article>
+      </section>
+    </>
   );
 };
 
