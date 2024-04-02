88462b7c1ec0eefb365bd41e89318db33a20d778
Bruno Madozzo Romay <108587068+brunomry@users.noreply.github.com>
2024-04-02 19:30:51 -0300

    agregue atributo active en 3ra opcion del breadcrumb

--- src/components/pages/Pedido.jsx
+++ src/components/pages/Pedido.jsx
@@ -10,7 +10,7 @@ const Pedido = () => {
       <Breadcrumb className='pt-4 pt-md-5 pb-2'>
         <BreadcrumbItem as={Link} to="/">Inicio</BreadcrumbItem>
         <BreadcrumbItem as={Link} to="/menu">Menu</BreadcrumbItem>
-        <BreadcrumbItem as={Link} className="text-secondary" to="/pedido">Mi pedido</BreadcrumbItem>
+        <BreadcrumbItem as={Link} active to="/pedido">Mi pedido</BreadcrumbItem>
       </Breadcrumb>
       <div className='py-2 pb-md-4'>
         <h1 className='orderTitle fw-bold'>- Mi pedido -</h1>
