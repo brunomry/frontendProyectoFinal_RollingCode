abd428edbc3def625404a661827749a03def6aaa
Bruno Madozzo Romay <108587068+brunomry@users.noreply.github.com>
2024-04-02 19:14:12 -0300

    Agregue cp Link de react-router con la ruta en opciones de breadcrumb. Modifique padding vertical al breadcrumb y h1 para completar los cambios solicitados

--- src/components/pages/Pedido.jsx
+++ src/components/pages/Pedido.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import ListaProductosUsuario from './order/user/ListaProductosUsuario';
 import ResumenPedido from './order/user/ResumenPedido';
 import '../../styles/pedido.css';
 import { Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Pedido = () => {
   return (
     <div className='container'>
-      <Breadcrumb className='my-3'>
-        <BreadcrumbItem href='/inicio'>Inicio</BreadcrumbItem>
-        <BreadcrumbItem href='/menu'>Menu</BreadcrumbItem>
-        <BreadcrumbItem href='#'>Mi pedido</BreadcrumbItem>
+      <Breadcrumb className='pt-4 pt-md-5 pb-2'>
+        <BreadcrumbItem as={Link} to="/">Inicio</BreadcrumbItem>
+        <BreadcrumbItem as={Link} to="/menu">Menu</BreadcrumbItem>
+        <BreadcrumbItem as={Link} className="text-secondary" to="/pedido">Mi pedido</BreadcrumbItem>
       </Breadcrumb>
-      <div className='p-4'>
-        <h1 className='orderTitle fw-bold'>Mi pedido</h1>
+      <div className='py-2 pb-md-4'>
+        <h1 className='orderTitle fw-bold'>- Mi pedido -</h1>
       </div>
       <ListaProductosUsuario />
       <ResumenPedido />
