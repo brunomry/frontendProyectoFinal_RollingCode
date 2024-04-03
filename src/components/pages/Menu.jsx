4753098ebf12960096912d222b7cd498f7f15ee0
Fernando Herrera <herrera770@outlook.es>
2024-04-03 13:09:17 -0300

    se modificaron los botones selectores de categorías

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -12,29 +12,35 @@ const Menu = () => {
         <p className="fw-bold bannerText pb-3">
           ¡Descubre nuestro festín de sabores!
         </p>
-        <a className="bannerBTN text-center py-3 text-decoration-none" href="">DESCARGAR MENÚ</a>
+        <a className="bannerBTN text-center py-3 text-decoration-none" href="">
+          DESCARGAR MENÚ
+        </a>
       </section>
       <div className="border-bottom border-secondary d-flex justify-content-center py-3 pe-2 menuCategoryContainer position-sticky top-0">
-        <Button className="me-3" variant="warning">
-          <a className="link-dark text-decoration-none" href="#pizzas">
-            Pizzas
-          </a>
-        </Button>
-        <Button className="me-3" variant="warning">
-          <a className="link-dark text-decoration-none" href="#hamburguesas">
-            Hamburguesas
-          </a>
-        </Button>
-        <Button className="me-3" variant="warning">
-          <a className="link-dark text-decoration-none" href="#pastas">
-            Pastas
-          </a>
-        </Button>
-        <Button variant="warning">
-          <a className="link-dark text-decoration-none" href="#empanadas">
-            Empanadas
-          </a>
-        </Button>
+        <a
+          className="link-dark text-decoration-none btn btn-warning me-2"
+          href="#pizzas"
+        >
+          Pizzas
+        </a>
+        <a
+          className="link-dark text-decoration-none btn btn-warning me-2"
+          href="#hamburguesas"
+        >
+          Hamburguesas
+        </a>
+        <a
+          className="link-dark text-decoration-none btn btn-warning me-2"
+          href="#pastas"
+        >
+          Pastas
+        </a>
+        <a
+          className="link-dark text-decoration-none btn btn-warning me-2"
+          href="#empanadas"
+        >
+          Empanadas
+        </a>
       </div>
       <Form className="d-flex justify-content-center my-3 px-2">
         <Form.Group className="mb-3 searchForm" controlId="buscarMenu">
@@ -58,7 +64,10 @@ const Menu = () => {
         </Row>
       </Container>
       <Container className="mb-5">
-        <div className="categoryBurger d-flex align-items-center" id="hamburguesas">
+        <div
+          className="categoryBurger d-flex align-items-center"
+          id="hamburguesas"
+        >
           <h2 className="categoryTitle">Hamburguesas</h2>
         </div>
         <Row>
@@ -69,7 +78,7 @@ const Menu = () => {
       </Container>
       <Container className="mb-5">
         <div id="pastas">
-        <h2 className="categoryTitle">Pastas</h2>
+          <h2 className="categoryTitle">Pastas</h2>
         </div>
         <Row>
           <CardProducto></CardProducto>
@@ -79,7 +88,7 @@ const Menu = () => {
       </Container>
       <Container className="mb-5">
         <div id="empanadas">
-        <h2 className="categoryTitle">Empanadas</h2>
+          <h2 className="categoryTitle">Empanadas</h2>
         </div>
         <Row>
           <CardProducto></CardProducto>
