3c5ae5f2841163c1e6279462a48dc6184b92a680
Fernando Herrera <herrera770@outlook.es>
2024-04-02 23:29:55 -0300

    se agregaron media queries para todas las resoluciones y fuéron reorganizados los íconos

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,47 +4,63 @@ import "../../styles/footer.css";
 import logo from "../../assets/logo.png";
 const Footer = () => {
   return (
-    <Container fluid className="footerContainer py-3">
+    <Container fluid className="footerContainer py-5">
       <Row>
-        <Col className="d-flex align-items-center">
+        <Col md={12} lg={3} className="d-flex align-items-center justify-content-center mb-3">
           <img
             src={logo}
             alt="Logo de ambiente bohemio"
             className="footerIMG"
           />
         </Col>
-        <Col>
-            <h4 className="footerTitleSection">Opciones</h4>
-            <p>Inicio</p>
-            <p>Menú</p>
-            <p>Nosotros</p>
-            <p>Contacto</p>
+        <Col className="d-none d-md-block">
+          <h4 className="footerTitleSection">Opciones</h4>
+          <p>Inicio</p>
+          <p>Menú</p>
+          <p>Nosotros</p>
+          <p>Contacto</p>
         </Col>
-        <Col>
-            <h4 className="footerTitleSection">Información</h4>
-            <p>Políticas de privacidad</p>
-            <p>Legal</p>
-            <p>Promociones</p>
-            <p>Horarios de atención</p>
+        <Col xs={12} md={3} className="containerInformation">
+          <h4 className="footerTitleSection">Información</h4>
+          <p>Políticas de privacidad</p>
+          <p>Legal</p>
+          <p>Promociones</p>
+          <p>Horarios de atención</p>
         </Col>
-        <Col>
-            <h4 className="footerTitleSection">Contacto</h4>
-            <p><i className="fa-brands fa-whatsapp fa-xl"></i> 123-1234567</p>
-            <p><i className="fa-solid fa-location-dot fa-xl"></i> Gral. Paz 576</p>
-            <h4>Suscríbete para recibir más información</h4>
-            <Form className="contactForm d-flex">
-                <Form.Group>
-                    <Form.Control type="email" placeholder="ejemplo@correo.com"/>
-                </Form.Group>
-                <Button className="formBTN" variant="warning" type="submit">Enviar</Button>
-            </Form>
-        </Col>
-        {/* <Col className="d-flex flex-column mt-5">
-            <a className="mb-3" href=""><i class="fa-brands fa-x-twitter fa-2xl link-light"></i></a>
-            <a className="mb-3" href=""><i class="fa-brands fa-facebook-f fa-2xl link-light"></i></a>
-            <a href=""><i class="fa-brands fa-instagram fa-2xl link-light"></i></a>
-        </Col> */}
+        <Col xs={12} md={3} className="containerContact">
+          <h4 className="footerTitleSection">Contacto</h4>
+          <div className="d-flex justify-content-between">
+            <div>
+              <p>
+                <i className="fa-brands fa-whatsapp fa-xl"></i> 123-1234567
+              </p>
+              <p>
+                <i className="fa-solid fa-location-dot fa-xl"></i> Gral. Paz 576
+              </p>
+            </div>
+            <div>
+              <a href="https://www.instagram.com/">
+                <i className="fa-brands fa-instagram fa-2xl me-md-2 me-lg-4 link-dark"></i>
+              </a>
+              <a href="https://www.facebook.com/">
+                <i className="fa-brands fa-x-twitter fa-2xl me-md-2 me-lg-4 link-dark"></i>
+              </a>
+              <a href="https://twitter.com/home">
+                <i className="fa-brands fa-facebook-f fa-2xl link-dark"></i>
+              </a>
+            </div>
+          </div>
 
+          <h4>Suscríbete para recibir más información</h4>
+          <Form className="contactForm d-flex">
+            <Form.Group>
+              <Form.Control type="email" placeholder="ejemplo@correo.com" />
+            </Form.Group>
+            <Button className="formBTN" variant="warning" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </Col>
       </Row>
     </Container>
   );
