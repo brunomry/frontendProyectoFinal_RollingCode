58b0ea605bcc411569bfbc5fba27e6496a87bffe
Fernando Herrera <herrera770@outlook.es>
2024-04-03 20:54:58 -0300

    se creó y estilizó el modal

--- src/components/pages/product/ModalDetalleProducto.jsx
+++ src/components/pages/product/ModalDetalleProducto.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import "../../../styles/modalDetalleProducto.css";
 
 const ModalDetalleProducto = ({
@@ -10,8 +10,7 @@ const ModalDetalleProducto = ({
   handleShow,
 }) => {
   return (
-    <>
-      <Modal show={show} onHide={handleClose} centered>
+      <Modal show={show} onHide={handleClose} centered className="modal">
         <Modal.Header className="modalHeaderIMGContainer">
           <img
             src="https://images.pexels.com/photos/1166120/pexels-photo-1166120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -20,19 +19,26 @@ const ModalDetalleProducto = ({
           />
         </Modal.Header>
         <Modal.Body>
-          <h4>Nombre del producto</h4>
-          <p className="fs-3 text-success">$55555</p>
+          <h4 className="mb-1">Nombre del producto</h4>
+          <p className="text-success price mb-1">$55555</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
             aspernatur adipisci harum aliquid nam distinctio quo modi magni odio
             dignissimos?
           </p>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Control type="number" placeholder="cantidad" required />
+            </Form.Group>
+            <div className="d-flex justify-content-between">
+              <Button className="btn btn-secondary closeBTN" onClick={handleClose}>Cerrar</Button>
+              <Button className="addBTN btn-success" type="submit">
+                <i className="fa-solid fa-plus fa-lg"></i> Agregar a Mi Pedido
+              </Button>
+            </div>
+          </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button className="addBTN"><i class="fa-solid fa-plus fa-lg"></i> Agregar a Mi Pedido</Button>
-        </Modal.Footer>
       </Modal>
-    </>
   );
 };
 
