952a17275fc35073b7c2161b598ff032c43a89a7
Fernando Herrera <herrera770@outlook.es>
2024-04-03 16:32:36 -0300

    se hizo una base del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Modal } from "react-bootstrap";
+import ModalDetalleProducto from "./components/pages/product/ModalDetalleProducto";
+import { useState } from "react";
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
+      <Button variant="warning" onClick={handleShow}>
+        {" "}
+        Ver Detalles
+      </Button>
+      <ModalDetalleProducto
+        show={show}
+        setShow={setShow}
+        useState={useState}
+        handleClose={handleClose}
+        handleShow={handleShow}
+      ></ModalDetalleProducto>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/product/ModalDetalleProducto.jsx
+++ src/components/pages/product/ModalDetalleProducto.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import "../../../styles/modalDetalleProducto.css";
 
-const ModalDetalleProducto = () => {
+const ModalDetalleProducto = ({
+  show,
+  setShow,
+  useState,
+  handleClose,
+  handleShow,
+}) => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header className="modalHeaderIMGContainer">
+          <img
+            src="https://images.pexels.com/photos/1166120/pexels-photo-1166120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="imagen del producto"
+            className="modalIMG"
+          />
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Nombre del producto</h4>
+          <p className="fs-3 text-success">$55555</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+            aspernatur adipisci harum aliquid nam distinctio quo modi magni odio
+            dignissimos?
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="addBTN"><i class="fa-solid fa-plus fa-lg"></i> Agregar a Mi Pedido</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
