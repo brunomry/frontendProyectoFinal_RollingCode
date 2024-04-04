914bfd5267d8b9148bc8152beeb39196e1ffd281
Fernando Herrera <herrera770@outlook.es>
2024-04-04 00:46:21 -0300

    el formulario para agregar cantidad ahora es un button para sumar o restar

--- src/components/pages/product/ModalDetalleProducto.jsx
+++ src/components/pages/product/ModalDetalleProducto.jsx
@@ -10,35 +10,48 @@ const ModalDetalleProducto = ({
   handleShow,
 }) => {
   return (
-      <Modal show={show} onHide={handleClose} centered className="modal">
-        <Modal.Header className="modalHeaderIMGContainer">
-          <img
-            src="https://images.pexels.com/photos/1166120/pexels-photo-1166120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="imagen del producto"
-            className="modalIMG"
-          />
-        </Modal.Header>
-        <Modal.Body className="pt-1">
-          <h4 className="mb-1">Nombre del producto</h4>
-          <p className="text-success price mb-1 fw-bold">$55555</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-            aspernatur adipisci harum aliquid nam distinctio quo modi magni odio
-            dignissimos?
-          </p>
-          <Form>
-            <Form.Group className="mb-3">
-              <Form.Control type="number" placeholder="cantidad" required />
-            </Form.Group>
-            <div className="d-flex containerBTN">
-              <Button className="btn btn-secondary closeBTN" onClick={handleClose}>Cerrar</Button>
-              <Button className="addBTN btn-success" type="submit">
-                <i className="fa-solid fa-plus fa-lg"></i> Agregar a Mi Pedido
+    <Modal show={show} onHide={handleClose} centered className="modal">
+      <Modal.Header className="modalHeaderIMGContainer">
+        <img
+          src="https://images.pexels.com/photos/1166120/pexels-photo-1166120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="imagen del producto"
+          className="modalIMG"
+        />
+      </Modal.Header>
+      <Modal.Body className="pt-1">
+        <h4 className="mb-1">Nombre del producto</h4>
+        <p className="text-success price mb-1 fw-bold">$55555</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+          aspernatur adipisci harum aliquid nam distinctio quo modi magni odio
+          dignissimos?
+        </p>
+        <Form>
+          <Form.Group className="mb-3">
+            <div className="text-center align-middle quantityProductos">
+              <Button className="mx-3 mx-sm-3" variant="secondary">
+                -
+              </Button>
+              {1}
+              <Button className="mx-3 mx-sm-3" variant="secondary">
+                +
               </Button>
             </div>
-          </Form>
-        </Modal.Body>
-      </Modal>
+          </Form.Group>
+          <div className="d-flex containerBTN">
+            <Button
+              className="btn btn-secondary closeBTN"
+              onClick={handleClose}
+            >
+              Cerrar
+            </Button>
+            <Button className="addBTN btn-success" type="submit">
+              <i className="fa-solid fa-plus fa-lg"></i> Agregar a Mi Pedido
+            </Button>
+          </div>
+        </Form>
+      </Modal.Body>
+    </Modal>
   );
 };
 
