f04f34c40119c1f4b3fd703a7e20ae1ec9fe97eb
Elias Juarez <juarezelias83@gmail.com>
2024-04-03 23:34:59 -0300

    se cambiaron los botones de ingresar, se agrego un enlace de contraseña olvidada y otro enlace para ir al registro

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Button, Form, FormCheck } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import imgLogin from "../../assets/imgLogin.jpeg"
 import "../../styles/login.css"
@@ -7,11 +7,11 @@ const Login = () => {
   return (
     <div className="d-flex">
       <section className="d-none d-sm-block">
-        <img src={imgLogin} alt="imagen del login" className="imgRegistro" />
+        <img src={imgLogin} alt="imagen del login" className="imgLogin" />
       </section>
-      <section className="sectionRegistro bg-white">
+      <section className="sectionLogin bg-white">
         <div className="text-center">
-          <h1 className="mt-3">Inicia sesión</h1>
+          <h1 className="my-4">Inicia sesión</h1>
         </div>
         <Form className="px-2 px-md-5 formText">
           <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -23,20 +23,17 @@ const Login = () => {
             <Form.Control type="password" placeholder="contraseña" />
           </Form.Group>
           <Button className="w-100 mb-3" variant="success" type="submit">
-            Registrarme
+            Ingresar
           </Button>
-          <div className="d-flex mb-3">
-            <FormCheck className="pe-2"></FormCheck>
-            <p>Recibir Descuentos y Promociones por Email</p>
-          </div>
-          <a href="#" className="btn btn-outline-danger w-100 mb-3">
-          <i className="fa-brands fa-google"></i> Registrarme con Google
+          <a href="#">¿Has olvidado tu contraseña?</a>
+          <a href="#" className="btn btn-outline-danger w-100 mb-3 mt-5">
+          <i className="fa-brands fa-google"></i> Ingresar con Google
           </a>
           <a href="#" className="btn btn-outline-primary w-100 mb-3 mb-md-5">
-          <i className="fa-brands fa-facebook"></i> Registrarme con Facebook
+          <i className="fa-brands fa-facebook"></i> Ingresar con Facebook
           </a>
           <p className="text-center">
-            ¿Ya tienes una cuenta? <Link to={"/login"}>Iniciar sesión</Link>
+            ¿Aún no te creaste una cuenta? <Link to={"/registro"}>Crear cuenta</Link>
           </p>
         </Form>
       </section>
