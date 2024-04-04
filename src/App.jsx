e1a97afd4a0d6fc523e141ff2b50fcffeee8d1dc
Leonel <leonelrcordero22@gmail.com>
2024-04-03 23:33:57 -0300

    Se realizo el maquetado del error404 y se agregaron los estilos correspondientes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error404 from './components/pages/Error404';
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/error404' element={<Error404></Error404>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
