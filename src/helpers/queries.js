20ae7c301283379cc97438bed6828985a61f4018
Fernando Herrera <herrera770@outlook.es>
2024-04-05 12:56:03 -0300

    se organizó el código

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -2,24 +2,21 @@ const URL_Productos = import.meta.env.VITE_API_PRODUCTOS;
 
 console.log(URL_Productos);
 
+export const leerProductosAPI = async () => {
+  try {
+    const respuesta = await fetch(URL_Productos);
+    const listaProductos = await respuesta.json();
+    return listaProductos;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const leerProductosAPI = async() =>{
-    try{
-        const respuesta = await fetch(URL_Productos);
-        const listaProductos = await respuesta.json();
-        console.log(listaProductos);
-        return listaProductos
-    }catch(error){
-        console.log(error);
-    }
-}
-
-export const obtenerProductoAPI = async (id) =>{
-    try{
-        const respuesta = await fetch(URL_Productos + '/' + id);
-        console.log(respuesta);
-        return respuesta;
-    }catch(error){
-        console.log(error);
-    }
-}
+export const obtenerProductoAPI = async (id) => {
+  try {
+    const respuesta = await fetch(URL_Productos + "/" + id);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
