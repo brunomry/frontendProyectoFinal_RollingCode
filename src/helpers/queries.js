805f777779cbeb736d09f5eff1468bf3a21a2e6b
brunomry <brunomadozzo@gmail.com>
2024-04-07 10:08:32 -0300

    Agregue funcion leerUsuariosAPI en queries.js para obtener la lista de usuarios desde la API

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,4 +1,5 @@
 const URL_Productos = import.meta.env.VITE_API_PRODUCTOS;
+const URL_Usuarios = import.meta.env.VITE_API_USUARIOS;
 
 export const leerProductosAPI = async () => {
   try {
@@ -14,6 +15,16 @@ export const obtenerProductoAPI = async (id) => {
   try {
     const respuesta = await fetch(URL_Productos + "/" + id);
     return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const leerUsuariosAPI = async () => {
+  try {
+    const respuesta = await fetch(URL_Usuarios);
+    const listaUsuarios = await respuesta.json();
+    return listaUsuarios;
   } catch (error) {
     console.log(error);
   }
