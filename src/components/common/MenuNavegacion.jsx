358a158bfb6835e6eeecb7442a04a2e66acbe994
Fernando Herrera <herrera770@outlook.es>
2024-04-09 20:47:54 -0300

    se agregaron condicionales a la renderizacion de elementos del navbar. Tambien se modificaron elementos en el css del navbar para editar el boton Mi pedido

--- src/components/common/MenuNavegacion.jsx
+++ src/components/common/MenuNavegacion.jsx
@@ -11,43 +11,118 @@ const MenuNavegacion = ({usuarioLogeado, setUsuarioLogeado}) => {
     <>
       <header className="p-2 d-none d-md-block d-md-flex d-lg-flex d-xl-flex d-xxl-flex justify-content-between align-items-center">
         <div>
-          <Link to="*" target="_blank"><i className="fa-brands fa-facebook-f fa-2xl mx-3 link-primary"> </i></Link>
-          <Link to="*" target="_blank"><i className="fa-brands fa-instagram fa-2xl me-3 link-danger"></i></Link>
-          <Link to="*" target="_blank"><i className="fa-brands fa-x-twitter fa-2xl link-dark"></i></Link>
+          <Link to="*" target="_blank">
+            <i className="fa-brands fa-facebook-f fa-2xl mx-3 link-primary">
+              {" "}
+            </i>
+          </Link>
+          <Link to="*" target="_blank">
+            <i className="fa-brands fa-instagram fa-2xl me-3 link-danger"></i>
+          </Link>
+          <Link to="*" target="_blank">
+            <i className="fa-brands fa-x-twitter fa-2xl link-dark"></i>
+          </Link>
         </div>
         <div>
-          <Link to="/"><img src={logo} alt="Logo ambiente bohemio" className="img-fluid logo" /></Link>
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Logo ambiente bohemio"
+              className="img-fluid logo"
+            />
+          </Link>
         </div>
         <div>
           <p className="me-3">
-            <i className="fa-brands fa-whatsapp fa-2xl link-success"></i> 123-1234567
+            <i className="fa-brands fa-whatsapp fa-2xl link-success"></i>{" "}
+            123-1234567
           </p>
         </div>
       </header>
-        <Navbar expand="md" className="navLinks navbarBackground">
-          <Container fluid>
-            <Navbar.Brand className="d-block d-md-none" as={Link} to="/">
-              <img src={logo} alt="Logo ambiente bohemio" className="img-fluid logoMobile" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mx-auto align-items-center">
-                <NavLink className="navLink nav-link text-center" to="/"><b>INICIO</b></NavLink>
-                <NavLink className="navLink nav-link text-center" to="/menu"><b>MENÚ</b></NavLink>
-                <NavLink className="navLink nav-link text-center" to="/nosotros"><b>NOSOTROS</b></NavLink>
-                <NavLink className="navLink nav-link text-center" to="/contacto"><b>CONTACTO</b></NavLink>
-                <NavLink className="navLink nav-link d-none text-center" to="/administracion"><b>ADMINISTRACIÓN</b></NavLink>
-                <NavLink className="navLink nav-link text-center" to="/miPedido">
-                  {" "}
-                  <i className="fa-solid fa-cart-shopping fa-xl"></i><b>MI PEDIDO</b>
-                </NavLink>
-                <NavLink className="navLinkBTN nav-link border border-1 border-dark rounded-2 mx-2 my-1 px-2" to="/login"><b>INICIAR SESIÓN</b></NavLink>
-                <NavLink className="navLinkBTN nav-link border border-1 border-dark rounded-2 mx-2 my-1 px-2" to="/registro"><b>REGISTRARME</b></NavLink>
-                <NavLink className="navLinkBTN nav-link d-none border border-1 border-dark rounded-2 me-2 my-1" to="#"><b>CERRAR SESIÓN</b></NavLink>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+      <Navbar expand="md" className="navLinks navbarBackground">
+        <Container fluid>
+          <Navbar.Brand className="d-block d-md-none" as={Link} to="/">
+            <img
+              src={logo}
+              alt="Logo ambiente bohemio"
+              className="img-fluid logoMobile"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto align-items-center">
+              <NavLink className="navLink nav-link text-center" to="/">
+                <b>INICIO</b>
+              </NavLink>
+              <NavLink className="navLink nav-link text-center" to="/menu">
+                <b>MENÚ</b>
+              </NavLink>
+              <NavLink className="navLink nav-link text-center" to="/nosotros">
+                <b>NOSOTROS</b>
+              </NavLink>
+              <NavLink className="navLink nav-link text-center" to="/contacto">
+                <b>CONTACTO</b>
+              </NavLink>
+              {usuarioLogeado.rol === "Administrador" && (
+                <>
+                  <NavLink
+                    className="navLink nav-link text-center"
+                    to="/administrador"
+                  >
+                    <b>ADMINISTRACIÓN</b>
+                  </NavLink>
+                  <button
+                    className="navLinkBTN nav-link border border-1 border-dark rounded-2 mx-2 my-1 px-2 fw-bold"
+                    onClick={cerrarSesion}
+                  >
+                    CERRAR SESION
+                  </button>
+                </>
+              )}
+              {usuarioLogeado.rol === "Usuario" && (
+                <>
+                  <NavLink
+                    className="navLink nav-link text-center"
+                    to="/miPedido"
+                  >
+                    <i className="fa-solid fa-cart-shopping fa-xl"></i>
+                    <b>MI PEDIDO</b>
+                  </NavLink>
+                  <button
+                    className="navLinkBTN nav-link border border-1 border-dark rounded-2 mx-2 my-1 px-2 fw-bold"
+                    onClick={cerrarSesion}
+                  >
+                    CERRAR SESION
+                  </button>
+                </>
+              )}
+              {usuarioLogeado.rol === undefined && (
+                <>
+                  <NavLink
+                    className="myOrderBTN nav-link text-center" 
+                    onClick={myOrderAlert}
+                  >
+                    <i className="fa-solid fa-cart-shopping fa-xl"></i>
+                    <b>MI PEDIDO</b>
+                  </NavLink>
+                  <NavLink
+                    className="navLinkBTN nav-link border border-1 border-dark rounded-2 mx-2 my-1 px-2"
+                    to="/login"
+                  >
+                    <b>INICIAR SESIÓN</b>
+                  </NavLink>
+                  <NavLink
+                    className="navLinkBTN nav-link border border-1 border-dark rounded-2 mx-2 my-1 px-2"
+                    to="/registro"
+                  >
+                    <b>REGISTRARME</b>
+                  </NavLink>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
