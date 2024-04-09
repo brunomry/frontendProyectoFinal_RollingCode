aebc05a86bb3fdf6b9c220b8e10d9361e7f3dc7e
Fernando Herrera <herrera770@outlook.es>
2024-04-09 20:53:28 -0300

    se agregó lógica de proteccion de rutas en RutasProtegidas

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
-const RutasProtegidas = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import {Navigate} from "react-router"
+const RutasProtegidas = ({children}) => {
+    const verificarUsuario = JSON.parse(sessionStorage.getItem('usuarioLogeado')) || undefined;
+    console.log(verificarUsuario)
+    if(verificarUsuario === undefined){
+        return <Navigate to={"/login"}></Navigate>
+    }else if (verificarUsuario.rol === "Administrador"){
+        return children
+    }else{
+        return <Navigate to={"/"}></Navigate>
+    }
 };
 
 export default RutasProtegidas;
