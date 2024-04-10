8e4f97a616e0895a432583e15415a164a516f0a1
Fernando Herrera <herrera770@outlook.es>
2024-04-09 21:06:54 -0300

    se eliminaron console.logs de RutasProtegidas

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Navigate} from "react-router"
 const RutasProtegidas = ({children}) => {
     const verificarUsuario = JSON.parse(sessionStorage.getItem('usuarioLogeado')) || undefined;
-    console.log(verificarUsuario)
     if(verificarUsuario === undefined){
         return <Navigate to={"/login"}></Navigate>
     }else if (verificarUsuario.rol === "Administrador"){
