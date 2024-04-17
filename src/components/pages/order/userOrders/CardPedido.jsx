a5c870281264abe1354c693bf60374ec87d1ffd4
brunomry <brunomadozzo@gmail.com>
2024-04-17 12:43:01 -0300

    Agregue estilos css y clases de bootstrap a cp CardPedido y cp MisPedidos ademas de mediaqueries para que sea responsive

--- src/components/pages/order/userOrders/CardPedido.jsx
+++ src/components/pages/order/userOrders/CardPedido.jsx
@@ -1,21 +1,24 @@
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "../../../../styles/cardPedido.css";
 
 const CardPedido = () => {
   return (
-    <Card className="cardPedido">
-      <Card.Header className="text-start">fechaPedido</Card.Header>
+    <div className="text-start card cardPedido border border-1 border-dark shadow">
+      <Card.Header>fechaPedido</Card.Header>
       <Card.Body>
-        <Card.Title className="text-success fw-bold text-start">
+        <p className="stateColor fw-bold mt-0 pt-0 text-center text-sm-start">
           Realizado
-        </Card.Title>
-        <Card.Text></Card.Text>
-        <div className="d-flex flex-column gap-2 flex-sm-row align-items-center justify-content-sm-center align-items-sm-start">
-          <Button variant="primary">Ver detalles del Pedido</Button>
-          <Button variant="secondary" as={Link} to="/*">Volver a comprar</Button>
+        </p>
+        <Card.Text className="text-center">
+          Total: <span className="text-danger">$9000</span> 
+        </Card.Text>
+        <div className="d-flex flex-column gap-2 flex-sm-row align-items-center justify-content-sm-center align-items-sm-start px-lg-5 pt-3">
+          <button className="btn border btnDetailsOrder text-white fw-bold">Ver Detalles del Pedido</button>
+          <Link className="btn border text-white btnBuy fw-bold" to="/*">Volver a Comprar</Link>
         </div>
       </Card.Body>
-    </Card>
+    </div>
   );
 };
 
--- src/components/pages/order/userOrders/MisPedidos.jsx
+++ src/components/pages/order/userOrders/MisPedidos.jsx
@@ -1,11 +1,11 @@
 import CardPedido from "./CardPedido";
-
+import "../../../../styles/cardPedido.css";
 
 const MisPedidos = () => {
   return (
-    <section className='text-center my-5 sectionTop'>
+    <section className='text-center my-5 sectionTop px-2 sectionOrdersUser'>
       <h1 className="mb-5">Mis pedidos realizados</h1>
-      <article className="d-flex flex-column gap-3 justify-content-center align-items-center">
+      <article className="d-flex flex-column gap-4 justify-content-center align-items-center containerOrdersUser">
         <CardPedido></CardPedido>
         <CardPedido></CardPedido>
         <CardPedido></CardPedido>
