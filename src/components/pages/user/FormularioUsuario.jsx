760bb7c5d90a12dac51f46c651d341d60407ce56
Elias Juarez <juarezelias83@gmail.com>
2024-04-17 21:01:31 -0300

    se creo el Form junbo con un h3 de Campo obligatorio y un label e input para el nombre completo

--- src/components/pages/user/FormularioUsuario.jsx
+++ src/components/pages/user/FormularioUsuario.jsx
@@ -1,3 +1,4 @@
+import { Form } from "react-bootstrap";
 import "../../../styles/administrador.css";
 
 const FormularioUsuario = ({titulo, boton}) => {
@@ -7,6 +8,21 @@ const FormularioUsuario = ({titulo, boton}) => {
         <h1 className="mt-5 titleColor">{titulo}</h1>
         <hr className="container-md text-start my-0 mb-4" />
       </div>
+      <Form className="my-4 mx-auto border border border-1 border-white formProduct px-3 px-sm-5 py-4 rounded-2">
+        <h3 className="mb-4">
+          Campo obligatorio
+          <span className="text-danger fw-bold">*</span>
+        </h3>
+        <Form.Group className='mb-3' controlId='formBasicName'>
+            <Form.Label className="fw-bold">Nombre completo:</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='nombre'
+            />
+            <Form.Text className='text-danger'>
+            </Form.Text>
+          </Form.Group>
+      </Form>
     </section>
   );
 };
