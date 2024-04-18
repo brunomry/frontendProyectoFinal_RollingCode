382904b7db1e3cebf2b7ef95a1660a893bec17e4
Leonel <leonelrcordero22@gmail.com>
2024-04-17 22:25:00 -0300

    Se creo la logica para filtrar los pedidos del usuario

--- src/components/pages/order/userOrders/MisPedidos.jsx
+++ src/components/pages/order/userOrders/MisPedidos.jsx
@@ -1,14 +1,38 @@
-import CardPedido from "./CardPedido";
-import "../../../../styles/cardPedido.css";
+import CardPedido from './CardPedido';
+import '../../../../styles/cardPedido.css';
+import { useEffect, useState } from 'react';
+import { leerPedidosAPI } from '../../../../helpers/queries';
+import Load from '../../../common/Load';
 
 const MisPedidos = () => {
+  const [listaPedidos, setListaPedidos] = useState([]);
+
+  const listarPedidos = async () => {
+    const idUsuario = JSON.parse(sessionStorage.getItem('usuarioLogeado'));
+    const respuesta = await leerPedidosAPI();
+    const pedidosFiltrados = respuesta.filter(
+      (pedido) => pedido.usuario === idUsuario.id
+    );
+    setListaPedidos(pedidosFiltrados);
+  };
+
+  useEffect(() => {
+    listarPedidos();
+  }, []);
+
+  if (listaPedidos.length == 0) {
+    return <Load />;
+  }
+
+  console.log(listaPedidos);
+
   return (
     <section className='text-center my-5 sectionTop px-2 sectionOrdersUser'>
-      <h1 className="mb-5">Mis pedidos realizados</h1>
-      <article className="d-flex flex-column gap-4 justify-content-center align-items-center containerOrdersUser">
-        <CardPedido></CardPedido>
-        <CardPedido></CardPedido>
-        <CardPedido></CardPedido>
+      <h1 className='mb-5'>Mis pedidos realizados</h1>
+      <article className='d-flex flex-column gap-4 justify-content-center align-items-center containerOrdersUser'>
+        {listaPedidos.map((pedidos) => (
+          <CardPedido key={pedidos._id} />
+        ))}
       </article>
     </section>
   );
