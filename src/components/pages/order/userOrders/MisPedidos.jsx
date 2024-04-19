557d9959003354b4680ce43e2f84d69ba4005d92
Bruno Madozzo Romay <108587068+brunomry@users.noreply.github.com>
2024-04-19 22:00:05 +0000

    Agregue import y state de spinner, quite import de load e if que retornaba h1 de No tienes pedidos realizados

--- src/components/pages/order/userOrders/MisPedidos.jsx
+++ src/components/pages/order/userOrders/MisPedidos.jsx
@@ -1,41 +1,38 @@
-import CardPedido from './CardPedido';
-import '../../../../styles/cardPedido.css';
-import { useEffect, useState } from 'react';
-import { leerPedidosAPI } from '../../../../helpers/queries';
-import Load from '../../../common/Load';
-import { Link } from 'react-router-dom';
+import CardPedido from "./CardPedido";
+import "../../../../styles/cardPedido.css";
+import { useEffect, useState } from "react";
+import { leerPedidosAPI } from "../../../../helpers/queries";
+import { Spinner } from "react-bootstrap";
+import { Link } from "react-router-dom/dist";
 
 const MisPedidos = () => {
   const [listaPedidos, setListaPedidos] = useState([]);
+  const [spinner, setSpinner] = useState(true);
 
   const listarPedidos = async () => {
-    const idUsuario = JSON.parse(sessionStorage.getItem('usuarioLogeado'));
+    const idUsuario = JSON.parse(sessionStorage.getItem("usuarioLogeado"));
     const respuesta = await leerPedidosAPI();
-    const pedidosDeUsuarios = respuesta.filter(
+    setSpinner(false);
+    const pedidosFiltrados = respuesta.filter(
       (pedido) => pedido.usuario === idUsuario.id
     );
-    const pedidosFiltrados = pedidosDeUsuarios.reverse();
-    setListaPedidos(pedidosFiltrados);
+    setListaPedidos(pedidosFiltrados.reverse());
   };
 
   useEffect(() => {
     listarPedidos();
   }, []);
 
-  if (listaPedidos.length == 0) {
-    return <h1 className='mt-5 text-center'>No tienes pedidos realizados</h1>;
-  }
-
   return listaPedidos.length > 0 ? (
-    <section className='text-center my-5 sectionTop px-2 sectionOrdersUser'>
-      <h1 className='mb-3'>Mis pedidos realizados</h1>
+    <section className="text-center my-5 sectionTop px-2 sectionOrdersUser">
+      <h1 className="mb-3">Mis pedidos realizados</h1>
       <Link
-        className='btn border border-1 mt-4 btn-warning border-dark px-4 fw-bold'
-        to='/menu'
+        className="btn border border-1 mt-4 btn-warning border-dark px-4 fw-bold"
+        to="/menu"
       >
         Ir al MENÚ
       </Link>
-      <article className='mt-4 d-flex flex-column gap-4 justify-content-center align-items-center containerOrdersUser'>
+      <article className="mt-4 d-flex flex-column gap-4 justify-content-center align-items-center containerOrdersUser">
         {listaPedidos.map((pedido) => (
           <CardPedido key={pedido._id} pedido={pedido} />
         ))}
@@ -45,18 +42,18 @@ const MisPedidos = () => {
     <>
       <div
         className={`mt-5 pt-5 d-flex align-items-start justify-content-center custom-spinner ${
-          !spinner ? 'd-none' : 'mb-5'
+          !spinner ? "d-none" : "mb-5"
         } `}
       >
-        {spinner && <Spinner animation='border' role='status'></Spinner>}
+        {spinner && <Spinner animation="border" role="status"></Spinner>}
       </div>
       {!spinner && (
-        <section className='text-center my-5 sectionTop px-2 sectionOrdersUser'>
-          <h1 className='mb-5'>Mis pedidos realizados</h1>
+        <section className="text-center my-5 sectionTop px-2 sectionOrdersUser">
+          <h1 className="mb-5">Mis pedidos realizados</h1>
           <p>No tienes pedidos realizados hasta el momento.</p>
           <Link
-            className='btn border border-1 mt-4 btn-warning border-dark px-4 fw-bold'
-            to='/menu'
+            className="btn border border-1 mt-4 btn-warning border-dark px-4 fw-bold"
+            to="/menu"
           >
             Ir al MENÚ
           </Link>
