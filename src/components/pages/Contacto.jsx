808a776a72b5f8a6ee80b819ed2dce76923809af
Elias Juarez <juarezelias83@gmail.com>
2024-04-19 05:00:39 -0300

    se agrego la seccion del banner en contacto

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Contacto = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="bannerContact d-flex justify-content-center align-items-center sectionTop">
+      <div className="bannerOpacity"></div>
+      <article className="bannerContent d-flex flex-column justify-content-center align-items-center gap-3 gap-md-4 gap-lg-3 gap-xl-4 pb-md-5 text-white">
+        <p className="mb-0 fw-bold bannerSubtitle text-center">Contáctanos</p>
+        <h1>¿Donde estamos?</h1>
+      </article>
+    </section>
+  );
 };
 
 export default Contacto;
