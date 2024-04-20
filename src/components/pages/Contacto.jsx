ba79ebe4433aa772afdcaaf011373721f1ff11b4
Elias Juarez <juarezelias83@gmail.com>
2024-04-20 02:49:50 -0300

    se añadieron media queries en el mapa para dispositivos lg y se agrego un rounded al iframe

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -29,6 +29,7 @@ const Contacto = () => {
           <div className="mb-4">
             <div className="containerMap w-100 d-flex align-items-center justify-content-center">
               <iframe
+              className="rounded-3"
                 src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Gral.%20Paz%20576,%20T4000%20San%20Miguel%20de%20Tucum%C3%A1n,%20Tucum%C3%A1n+(Ambiente%20Bohemio)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
               </iframe>
             </div>
