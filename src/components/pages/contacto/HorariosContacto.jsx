1ce0ffdd550c33e81a33f63e0cc77f29efefa437
Elias Juarez <juarezelias83@gmail.com>
2024-04-20 03:17:43 -0300

    se agregaron los horarios de atencion en el componente HorariosContacto

--- src/components/pages/contacto/HorariosContacto.jsx
+++ src/components/pages/contacto/HorariosContacto.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 
 const HorariosContacto = () => {
     return (
-        <div>
-            
+        <article className="containerSchedules text-center">
+        <h4 className="mb-3 border-2 rounded-5 px-4 border-bottom py-3  border-warning text-center mx-auto">
+          Horarios de Atención
+        </h4>
+        <div className="d-flex flex-column align-items-center">
+          <div className="mb-1 d-flex justify-content-between containerSchedule">
+            <span className="fw-bold me-2">Lunes</span>
+            <span>12:00 - 00:00</span>
+          </div>
+          <hr className="containerSchedule my-0" />
+          <div className="mb-1 d-flex justify-content-between containerSchedule">
+            <span className="fw-bold me-2">Martes</span>12:00 - 00:00
+          </div>
+          <hr className="containerSchedule my-0" />
+          <div className="mb-1 d-flex justify-content-between containerSchedule">
+            <span className="fw-bold me-2">Miércoles</span>12:00 - 00:00
+          </div>
+          <hr className="containerSchedule my-0" />
+          <div className="mb-1 d-flex justify-content-between containerSchedule">
+            <span className="fw-bold me-2">Jueves</span>12:00 - 00:00
+          </div>
+          <hr className="containerSchedule my-0" />
+          <div className="mb-1 d-flex justify-content-between containerSchedule">
+            <span className="fw-bold me-2">Viernes</span>12:00 - 00:00
+          </div>
+          <hr className="containerSchedule my-0" />
+          <div className="mb-1 d-flex justify-content-between containerSchedule">
+            <span className="fw-bold me-2">Sábado</span>12:00 - 00:00
+          </div>
+          <hr className="containerSchedule my-0" />
         </div>
+      </article>
     );
 };
 
