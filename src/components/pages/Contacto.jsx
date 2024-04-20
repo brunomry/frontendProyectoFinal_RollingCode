f1f8cc2c5c773734add8eca9c70493006dd6bff6
Elias Juarez <juarezelias83@gmail.com>
2024-04-20 02:40:35 -0300

    se agrego un iframe con un mapa en el componente Contacto

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -16,15 +16,24 @@ const Contacto = () => {
       </section>
       <section>
         <div className="w-100 d-flex flex-column align-items-center my-4 gap-3 gap-md-2 text-center">
-            <p>
-              <i className="fa-solid fa-location-dot fs-5"></i>
-              General Paz 576, San Miguel de Tucumán, Tucumán
-            </p>
-            <p title="WhatsApp">
-              <i className="fa-brands fa-whatsapp fa-2xl link-success"></i>{" "}
-              123-1234567
-            </p>
+          <p>
+            <i className="fa-solid fa-location-dot fs-5"></i>
+            General Paz 576, San Miguel de Tucumán, Tucumán
+          </p>
+          <p title="WhatsApp">
+            <i className="fa-brands fa-whatsapp fa-2xl link-success"></i>{" "}
+            123-1234567
+          </p>
         </div>
+        <article>
+          <div className="mb-4">
+            <div className="containerMap w-100 d-flex align-items-center justify-content-center">
+              <iframe
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Gral.%20Paz%20576,%20T4000%20San%20Miguel%20de%20Tucum%C3%A1n,%20Tucum%C3%A1n+(Ambiente%20Bohemio)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+              </iframe>
+            </div>
+          </div>
+        </article>
       </section>
     </>
   );
