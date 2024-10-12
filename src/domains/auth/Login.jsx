9659e9344e02b72c3a3d80cae63edd6f8daa2baf
Bruno Madozzo <brunomadozzo@gmail.com>
2024-10-12 13:37:57 -0300

    modifique pagina de login y sus estilos

--- src/domains/auth/Login.jsx
+++ src/domains/auth/Login.jsx
@@ -1,11 +1,10 @@
-import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import imgLogin from '../../assets/imgLogin.webp';
-import './styles/login.css';
-import { useForm } from 'react-hook-form';
-import { leerUsuariosAPI, login } from '../../helpers/queries';
-import { useNavigate } from 'react-router';
-import Swal from 'sweetalert2';
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./styles/login.css";
+import { useForm } from "react-hook-form";
+import { leerUsuariosAPI, login } from "../../helpers/queries";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 
 const Login = ({ setUsuarioLogeado }) => {
   const {
@@ -19,7 +18,7 @@ const Login = ({ setUsuarioLogeado }) => {
   const onSubmit = async (usuario) => {
     let timerInterval;
     Swal.fire({
-      title: 'Iniciando sesión',
+      title: "Iniciando sesión",
       html: `Validando datos`,
       timer: 1300,
       timerProgressBar: true,
@@ -35,17 +34,20 @@ const Login = ({ setUsuarioLogeado }) => {
       if (result.dismiss === Swal.DismissReason.timer) {
       }
     });
-    const usuarioLowerCase = {correo: usuario.correo.toLowerCase(), clave: usuario.clave}
+    const usuarioLowerCase = {
+      correo: usuario.correo.toLowerCase(),
+      clave: usuario.clave,
+    };
     const respuesta = await login(usuarioLowerCase);
     if (respuesta.status === 200) {
       const listaUsuarios = await leerUsuariosAPI();
       const usuarioBuscado = listaUsuarios.find(
         (u) => u.correo === usuarioLowerCase.correo
       );
-      if (usuarioBuscado.rol === 'Administrador' && usuarioBuscado.estado) {
+      if (usuarioBuscado.rol === "Administrador" && usuarioBuscado.estado) {
         const datos = await respuesta.json();
         sessionStorage.setItem(
-          'usuarioLogeado',
+          "usuarioLogeado",
           JSON.stringify({
             id: usuarioBuscado._id,
             correo: usuarioBuscado.correo,
@@ -60,16 +62,16 @@ const Login = ({ setUsuarioLogeado }) => {
           token: datos.token,
         });
 
-        navegacion('/administrador');
+        navegacion("/administrador");
         Swal.fire({
-          icon: 'success',
-          title: 'Sesion iniciada como administrador',
+          icon: "success",
+          title: "Sesion iniciada como administrador",
           text: `Bienvenido ${usuarioBuscado.nombreCompleto}`,
         });
       }
-      if (usuarioBuscado.rol === 'Usuario' && usuarioBuscado.estado) {
+      if (usuarioBuscado.rol === "Usuario" && usuarioBuscado.estado) {
         sessionStorage.setItem(
-          'usuarioLogeado',
+          "usuarioLogeado",
           JSON.stringify({
             id: usuarioBuscado._id,
             correo: usuarioBuscado.correo,
@@ -81,122 +83,122 @@ const Login = ({ setUsuarioLogeado }) => {
           correo: usuarioBuscado.correo,
           rol: usuarioBuscado.rol,
         });
-        navegacion('/');
+        navegacion("/");
         Swal.fire({
-          icon: 'success',
-          title: 'Sesion iniciada',
+          icon: "success",
+          title: "Sesion iniciada",
           text: `Bienvenido ${usuarioBuscado.nombreCompleto}`,
         });
       }
 
       if (!usuarioBuscado.estado) {
         Swal.fire({
-          icon: 'error',
-          title: 'Usuario suspendido.',
-          text: 'En este momento no puedes iniciar sesion.',
+          icon: "error",
+          title: "Usuario suspendido.",
+          text: "En este momento no puedes iniciar sesion.",
         });
       }
     } else {
       Swal.fire({
-        icon: 'error',
-        title: 'No se pudo iniciar sesion',
-        text: 'Los datos ingresados no son correctos o esta deshabilitado.',
+        icon: "error",
+        title: "No se pudo iniciar sesion",
+        text: "Los datos ingresados no son correctos o esta deshabilitado.",
       });
     }
   };
 
   return (
-    <div className='d-flex sectionTop'>
-      <section className='d-none d-sm-block'>
-        <img
-          src={imgLogin}
-          alt='mesero sirviendo a clientes'
-          title='mesero sirviendo a clientes'
-          className='imgLogin h-100'
-        />
-      </section>
-      <section className='sectionLogin bg-white'>
-        <div className='text-center'>
-          <h1 className='my-4'>Inicia sesión</h1>
-        </div>
+    <section className="d-flex sectionTop px-2 containerLogin py-5 d-flex justify-content-center">
+      <div className="containerFormLogin py-xl-5 rounded-5 text-white d-flex flex-column  gap-3">
+        <h1 className="my-4 text-center">Inicia sesión</h1>
         <Form
-          className='px-2 px-md-5 pb-2 formText'
+          className="px-3 px-md-5 pb-2 text-white formText"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label>Email:</Form.Label>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="emailLogin">Correo electrónico</Form.Label>
             <Form.Control
-              type='email'
-              placeholder='email'
-              {...register('correo', {
-                required: 'El correo es obligatorio',
+              title="Ingresa tu correo eléctrónico"
+              type="email"
+              id="emailLogin"
+              className="py-xl-2 input inputFormLogin"
+              placeholder="nombre@gmail.com"
+              {...register("correo", {
+                required: "El correo es obligatorio",
                 minLenght: {
                   value: 3,
-                  message: 'El correo debe contener al menos 3 carácteres',
+                  message: "El correo debe contener al menos 3 carácteres",
                 },
                 maxLength: {
                   value: 265,
-                  message: 'El correo debe contener como máximo 265 carácteres',
+                  message: "El correo debe contener como máximo 265 carácteres",
                 },
                 pattern: {
                   value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-                  message: 'Ingrese una direccion de correo válida',
+                  message: "Ingrese una dirección de correo válida",
                 },
               })}
             />
-            <Form.Text className='text-danger'>
+            <Form.Text className="text-warning">
               {errors.correo?.message}
             </Form.Text>
           </Form.Group>
-          <Form.Group className='mb-3' controlId='formBasicPassword'>
-            <Form.Label>Contraseña:</Form.Label>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="passwordLogin">Contraseña</Form.Label>
             <Form.Control
-              type='password'
-              placeholder='contraseña'
-              {...register('clave', {
-                required: 'La clave es obligatoria',
+              title="Ingresa tu contraseña"
+              className="py-xl-2 input inputFormLogin"
+              type="password"
+              id="passwordLogin"
+              placeholder="clave123"
+              {...register("clave", {
+                required: "La contraseña es obligatoria",
                 minLength: {
                   value: 8,
-                  message: 'La clave debe tener al menos 8 carácteres',
+                  message: "La contraseña debe tener al menos 8 carácteres",
                 },
                 maxLength: {
                   value: 16,
-                  message: 'La clave debe tener como máximo 16 carácteres',
+                  message: "La contraseña debe tener como máximo 16 carácteres",
                 },
                 pattern: {
                   value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
                   message:
-                    'La clave debe tener al menos una letra minúscula y una letra mayúscula',
+                    "La contraseña debe tener al menos 1 letra minúscula y 1 letra mayúscula",
                 },
               })}
             />
-            <Form.Text className='text-danger'>
+            <Form.Text className="text-warning">
               {errors.clave?.message}
             </Form.Text>
           </Form.Group>
-          <Button className='w-100 mb-3' variant='success' type='submit'>
+          <button
+            className="w-100 mb-3 btnLogin text-white w-100 py-2 rounded-0 border-0"
+            type="submit"
+          >
             Ingresar
-          </Button>
-          <Link to='./error404'>¿Has olvidado tu contraseña?</Link>
-          <Link
-            to='./error404'
-            className='btn btn-outline-danger w-100 mb-3 mt-5'
-          >
-            <i className='fa-brands fa-google'></i> Ingresar con Google
-          </Link>
-          <Link
-            to='./error404'
-            className='btn btn-outline-primary w-100 mb-3 mb-md-5'
-          >
-            <i className='fa-brands fa-facebook'></i> Ingresar con Facebook
-          </Link>
-          <p className='text-center'>
-            ¿Aún no te creaste una cuenta?{' '}
-            <Link to={'/registro'}>Crear cuenta</Link>
-          </p>
+          </button>
+          <div className="d-flex flex-column w-100">
+            <Link to="./error404" className="text-white text-center">
+              ¿Has olvidado tu contraseña?
+            </Link>
+            <Link
+              to="./error404"
+              className=" text-center optionGoogle rounded-2 px-2 py-2 w-100 mb-3 mt-5"
+            >
+              <i className="fa-brands fa-google"></i>{" "}
+              <span>Ingresar con Google</span>
+            </Link>
+            <p className="text-center">
+              ¿Aún no tienes una cuenta?{" "}
+              <Link to={"/registro"} className="text-white">
+                Crear cuenta
+              </Link>
+            </p>
+          </div>
         </Form>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
