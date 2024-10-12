d803b539b27469bebd77ce4a4c3523b102f125a4
Bruno Madozzo <brunomadozzo@gmail.com>
2024-10-12 01:03:19 -0300

    agregue color de fondo a card de miembro en pagina equipo

--- src/pages/Equipo/Equipo.jsx
+++ src/pages/Equipo/Equipo.jsx
@@ -6,7 +6,7 @@ const Equipo = () => {
   return (
     <section className="container sectionTop text-white py-5">
       <h1 className="text-center m-0 mt-4 fw-bold">Equipo de Desarrollo</h1>
-      <article className="px-2 containerMembers d-flex justify-content-md-center flex-md-wrap gap-2 py-5">
+      <article className="px-2 containerMembers d-flex justify-content-md-center flex-md-wrap gap-3 py-5">
         {equipo.map((miembro) => (
           <CardMember key={miembro.id} miembro={miembro}></CardMember>
         ))}
